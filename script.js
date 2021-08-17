055b7c315f959efc9d26a1752ea1567a969b4498
JidNation <jidnation1074@gmail.com>
2021-08-17 14:04:30 -0400

    initail state

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    //Delaying the display time.
+
+    setTimeout(() => {
+
+        const nameEl = document.getElementById('my-name');
+        const name = "olatunji babajide";
+
+        nameEl.innerText = name;
+    }, 1000);
+}
